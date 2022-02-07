ab0fd0375f4a96a8c4aa8af4091009504f2bcb80
Igor Cavalcante <igor.cavalcante96@gmail.com>
2022-02-07 16:28:03 -0300

    RestTemplate - POST

--- src/main/java/academy/devdojo/springboot2essentials/client/SpringClient.java
+++ src/main/java/academy/devdojo/springboot2essentials/client/SpringClient.java
@@ -4,7 +4,10 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
@@ -35,5 +38,22 @@ public class SpringClient {
 
         log.info(exchange.getBody());
 
+        // Anime kingdom = Anime.builder().name("Kingdom").build();
+        // Anime kingdomSaved = new
+        // RestTemplate().postForObject("http://localhost:8080/animes", kingdom,
+        // Anime.class);
+        // log.info("saved anime {}", kingdomSaved);
+
+        Anime samuraiChamploo = Anime.builder().name("Samurai Champloo").build();
+        ResponseEntity<Anime> samuraiChamplooSaved = new RestTemplate().exchange("http://localhost:8080/animes",
+                HttpMethod.POST, new HttpEntity<>(samuraiChamploo, createJsonHeader()), Anime.class);
+        log.info("saved anime {}", samuraiChamplooSaved);
+
+    }
+
+    private static HttpHeaders createJsonHeader() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return httpHeaders;
     }
 }
