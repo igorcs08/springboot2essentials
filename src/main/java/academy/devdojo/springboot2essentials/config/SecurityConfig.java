78a9b5abb82abce1ca016c5b276c819a41345bd2
Igor Cavalcante <igor.cavalcante96@gmail.com>
2022-02-10 17:19:04 -0300

    Spring Security parte 03 - Segurança a nível de métodos com PreAuthorize

--- src/main/java/academy/devdojo/springboot2essentials/config/SecurityConfig.java
+++ src/main/java/academy/devdojo/springboot2essentials/config/SecurityConfig.java
@@ -1,22 +1,25 @@
 package academy.devdojo.springboot2essentials.config;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import lombok.extern.log4j.Log4j2;
 
 @EnableWebSecurity
 @Log4j2
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
+                // csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                // .and()
                 .authorizeRequests()
                 .anyRequest()
                 .authenticated()
--- src/main/java/academy/devdojo/springboot2essentials/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2essentials/controller/AnimeController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -53,6 +54,7 @@ public class AnimeController {
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Anime> save(@RequestBody @Valid AnimePostRequestBody anime) {
         return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
     }
